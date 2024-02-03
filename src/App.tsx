e7924f1a67bf7572e1236c0419248eeb62faa987
artёm rudoy <art.rudy@gmail.com>
2024-02-03 23:38:59 +0200

    web form home task first ccommit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { FormEvent } from "react"
+import { FormEventHandler } from "react"
+import { FormHTMLAttributes } from "react"
+
+function App() {
+  
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
