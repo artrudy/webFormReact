a10218babf0abdd1d5e2edeb8d10e52fcd9206f9
artёm rudoy <art.rudy@gmail.com>
2024-02-04 23:52:40 +0200

    submit using formdata

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
-import React from "react"
-import { FormEvent } from "react"
-import { FormEventHandler } from "react"
-import { FormHTMLAttributes } from "react"
+import React, {useState, ChangeEvent, FormEvent} from "react"
+
+interface formData{
+  userName: string;
+  password: string;
+}
 
 function App() {
-  
 
+  const [formData, setFormData] = useState<formData>({
+    userName: '',
+    password: '',
+  });
+
+  const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  }
+
+  const handleSubmit = (e:FormEvent) => {
+    e.preventDefault();
+    console.log(formData);
+
+  }
+  
   return (
-    <>
-      
-    </>
+    <form onSubmit={handleSubmit}> 
+      <label>
+        Username:
+        <input type='text' name="userName" value={formData.userName} onChange={handleChange}/>
+      </label>
+      <label>
+        <input type="password" name="password" value={formData.password} onChange={handleChange}/>
+      </label>
+      <input type="submit" value='Submit' />
+    </form>
   )
 }
 
